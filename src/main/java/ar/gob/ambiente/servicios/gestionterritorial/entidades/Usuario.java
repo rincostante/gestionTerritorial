c1f391a060fb91325a161dea399d2979700fb643
Ruben Incostante <rincostante@ambiente.gob.ar>
2015-06-17 10:58:44 -0300

    Se completó la gestión de usuarios

--- src/main/java/ar/gob/ambiente/servicios/gestionterritorial/entidades/Usuario.java
+++ src/main/java/ar/gob/ambiente/servicios/gestionterritorial/entidades/Usuario.java
@@ -18,7 +18,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -52,12 +51,12 @@ public class Usuario implements Serializable {
     private String nombre;     
     
     /**
-     * Campo de texto que indica la clave de acceso del usuario
+     * Campo de texto que indica el nombre completo del usuario
      */        
-    @Column (nullable=false, length=50, unique=true)
+    @Column (nullable=false, length=100)
     @NotNull(message = "{entidades.fieldNotNullError}")
-    @Size(message = "{endidades.stringSizeError}", min = 1, max = 50)
-    private String calve; 
+    @Size(message = "{endidades.stringSizeError}", min = 1, max = 100)
+    private String nombreCompleto;   
     
     /**
      * Campo de tipo AdmEntidad que encapsula los datos propios para su trazabilidad.
@@ -65,35 +64,14 @@ public class Usuario implements Serializable {
     @OneToOne(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
     @NotNull(message = "{enitdades.objectNotNullError}") 
     private AdminEntidad admin;
-    
-    /**
-     * Este campo solo está en true cuando el usuario es registrado o
-     * su contraseña blanqueada. El sistema validará este campo en cada 
-     * inicio de sesión, en caso de ser verdadero redirecciona al cambio de contraseña,
-     * la que una vez actualizada permitirá al usuario reiniciar sesión y operar normalemente el sistema.
-     */
-    @Column(nullable=false)
-    @NotNull(message = "El campo 'primeraVez' no puede quedar vacío")
-    private boolean primeraVez = true;    
-    
-    /**
-     * Campo que muestra los Apellidos y nombres personales del docente
-     */
-    @Transient
-    String apYNom;
-    
-    /**
-     * Campo que muestra el número de documento personal del docente
-     */
-    @Transient
-    String documento;    
 
-    public boolean isPrimeraVez() {
-        return primeraVez;
+    
+    public String getNombreCompleto() {
+        return nombreCompleto;
     }
 
-    public void setPrimeraVez(boolean primeraVez) {
-        this.primeraVez = primeraVez;
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
     }
 
     /**
@@ -120,22 +98,6 @@ public class Usuario implements Serializable {
         this.rol = rol;
     }
 
-    public String getApYNom() {
-        return apYNom;
-    }
-
-    public void setApYNom(String apYNom) {
-        this.apYNom = apYNom;
-    }
-
-    public String getDocumento() {
-        return documento;
-    }
-
-    public void setDocumento(String documento) {
-        this.documento = documento;
-    }
-
     /**
      *
      * @return
@@ -150,22 +112,6 @@ public class Usuario implements Serializable {
      */
     public void setAdmin(AdminEntidad admin) {
         this.admin = admin;
-    }
-
-    /**
-     *
-     * @return
-     */
-    public String getCalve() {
-        return calve;
-    }
-
-    /**
-     *
-     * @param calve
-     */
-    public void setCalve(String calve) {
-        this.calve = calve;
     }
 
     /**
